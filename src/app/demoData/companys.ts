1a7a614c42fbbbef2d7e3674d358e002f3071194
malaotou <marrator@163.com>
2017-11-06 13:35:16 +0800

    change image url from abs to rel

--- src/app/demoData/companys.ts
+++ src/app/demoData/companys.ts
@@ -3,32 +3,32 @@ export const Companys=[
         id:'yamaxun',
         name:'亚马逊',
         description:'亚马逊冰箱服务团队',
-        avatar:'/assets/images/Amazon_48px.png',
+        avatar:'./assets/images/Amazon_48px.png',
         lastmsg:''
     },
     {   id:'hp',
         name:'惠普',
         description:'惠普空调服务团队',
-        avatar:'/assets/images/HP_48px.png',
+        avatar:'./assets/images/HP_48px.png',
         lastmsg:''
     },
     {   id:'maidanglao',
         name:'金拱门',
         description:'金拱门快餐售后服务团队',
-        avatar:'/assets/images/McDonald_48px.png',
+        avatar:'./assets/images/McDonald_48px.png',
         lastmsg:''
     },
     {   id:'guge',
         name:'谷歌',
         description:'谷歌售後服務團隊',
-        avatar:'/assets/images/Google_48px.png',
+        avatar:'./assets/images/Google_48px.png',
         lastmsg:''
     }
     ,
     {   id:'falali',
         name:'法拉利',
         description:'法拉利售後服務團隊',
-        avatar:'/assets/images/Ferrari_48px.png',
+        avatar:'./assets/images/Ferrari_48px.png',
         lastmsg:''
     }
 ]
